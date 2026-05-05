473a287f47f8e1655c6c90f464003aac9d3f181c
Vedanth Kumar <vedanthkumar17@outlook.com>
2026-05-05 21:46:27 +0530

    Day 2: remove unnecessary length variable

--- day2_parse_drivers.py
+++ day2_parse_drivers.py
@@ -3,10 +3,7 @@ import requests, json
 response = requests.get("https://api.openf1.org/v1/drivers?session_key=9158")
 data = response.json()
 print(json.dumps(data[0], indent=1))
-#prints the driver's information.
-
-length = len(data)
+#the above line prints the driver's information.
 
 for driver in data:
     print(f"{driver.get('name_acronym')} - {driver.get('full_name')} - {driver.get('team_name')}")
-
