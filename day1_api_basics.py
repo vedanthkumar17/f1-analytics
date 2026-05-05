fa89a5879abd12287d99610c1c00b5149422d9b5
Vedanth Kumar <vedanthkumar17@outlook.com>
2026-05-05 12:24:13 +0530

    Day 1: dictionary exercises and first API call

---
+++ day1_api_basics.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+response = requests.get("https://api.openf1.org/v1/drivers?session_key=9158")
+
+data = response.json()
+
+print(json.dumps(data, indent=2))
+
+#JSON is just a text file 
+#that stores data in a structured way that both humans and 
+#computers can read easily.
+
+"""
+The reason it exists is because when two computers talk to each other over 
+the internet they need to agree on a format. 
+JSON is that agreed format. 
+Every programming language in the world — Python, JavaScript, Java, 
+whatever — can read JSON. That's why it became the standard for APIs.
+"""
