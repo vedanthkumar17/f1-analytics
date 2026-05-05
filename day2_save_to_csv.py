1e851ec246b0ec07b7dbb0360f898bf0b61511b5
Vedanth Kumar <vedanthkumar17@outlook.com>
2026-05-05 17:02:46 +0530

    Day 2: save F1 driver data to CSV using Pandas

---
+++ day2_save_to_csv.py
@@ -0,0 +1,11 @@
+import requests
+import pandas
+
+response = requests.get("https://api.openf1.org/v1/drivers?session_key=9158")
+data = response.json()
+
+df = pandas.DataFrame(data)
+print(df.head())
+
+df.to_csv("drivers.csv", index = False)
+print("Saved to drivers.csv")
