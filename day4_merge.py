cb7871ece917f6b6e05898c71aa706e32f192d38
Vedanth Kumar <vedanthkumar17@outlook.com>
2026-05-07 20:15:49 +0530

    Day 4: merge driver and pit stop data

---
+++ day4_merge.py
@@ -0,0 +1,20 @@
+import requests
+import pandas as pd
+
+response1 = requests.get("https://api.openf1.org/v1/drivers?session_key=9158")
+response2 = requests.get("https://api.openf1.org/v1/pit?session_key=9158")
+
+data1 = response1.json()
+data2 = response2.json()
+
+df1 = pd.DataFrame(data1)
+df2 = pd.DataFrame(data2)
+
+df_merge = pd.merge(df1, df2, on = "driver_number")
+print(df_merge.head())
+print(f"Total rows: {len(df_merge)}")
+df_merge.to_csv("merged.csv", index=False)
+
+df_clean = df_merge.dropna(subset = ["pit_duration"])
+print(f"Real Pit Stops: {len(df_clean)}")
+df_clean.to_csv("merged.csv", index=False)
