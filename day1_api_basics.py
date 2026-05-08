16f91a3b8f899a267343bc65e1365ed7e84d4aa4
Vedanth Kumar <vedanthkumar17@gmail.com>
2026-05-08 18:14:58 +0530

    changed the session_key

--- day1_api_basics.py
+++ day1_api_basics.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-response = requests.get("https://api.openf1.org/v1/drivers?session_key=9158")
+response = requests.get("https://api.openf1.org/v1/drivers?session_key=9693")
 
 data = response.json()
 
