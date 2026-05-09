5cc080971bdb492e339dafcfd99238c3388a7ef2
Vedanth Kumar <vedanthkumar17@gmail.com>
2026-05-09 23:33:27 +0530

    team pit stop analysis, fastest average and individual stops

---
+++ analysis.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+df = pd.read_csv("merged.csv")
+
+print("Average pit stop by team:")
+
+print(df.groupby("team_name")["stop_duration"].mean(skipna = True).sort_values())
+
+print("\nFastest individual pit stop:")
+fastest_average_pit_stop = df.nsmallest(1, "stop_duration")[["team_name", "stop_duration"]]
+print(fastest_average_pit_stop)
