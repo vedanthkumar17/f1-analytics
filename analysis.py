b2c541b14cb7f1c1c3b51d191663bd5bbcff51ad
Vedanth Kumar <vedanthkumar17@gmail.com>
2026-05-11 19:21:45 +0530

    added the average pit stop duration per driver

--- analysis.py
+++ analysis.py
@@ -9,3 +9,5 @@ print(df.groupby("team_name")["stop_duration"].mean(skipna = True).sort_values()
 print("\nFastest individual pit stop:")
 fastest_average_pit_stop = df.nsmallest(1, "stop_duration")[["team_name", "stop_duration"]]
 print(fastest_average_pit_stop)
+
+print(df.groupby("full_name")["stop_duration"].mean(skipna=True).sort_values(ascending = False))
